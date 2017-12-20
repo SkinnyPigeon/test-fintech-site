66cbb4bbc4b4e420f69957f8684e9d8a886147e6
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-12-20 11:49:32 +0000

    initial commit

---
+++ client/src/app.js
@@ -0,0 +1,9 @@
+var MainView = require( './views/MainView.js' );
+
+window.onload = function() {
+  main();
+}
+
+var main = function() {
+  var MainView = new MainView();
+}
---
+++ client/src/views/MainView.js
@@ -0,0 +1,13 @@
+var MainView = function() {
+  this.start();
+};
+
+MainView.prototype = {
+
+  start: function() {
+    console.log( "Hello World" );
+  },
+
+};
+
+module.exports = MainView;
