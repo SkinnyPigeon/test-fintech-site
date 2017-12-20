afa6e19534e17d710e7b201f01e308a08b133cf6
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-12-20 12:02:09 +0000

    starting to get the existing companies

--- client/src/models/MainView.js
+++ client/src/models/MainView.js
@@ -1,8 +1,18 @@
+var ElementMaker = require( './ElementMaker.js' );
+
 var MainView = function() {
+  this.companies = [];
+
+  this.getCompanies();
   this.show();
 }
 
 MainView.prototype = {
+
+  getCompanies: function() {
+    
+  },
+
   show: function() {
     console.log( "Hello" );
   }
