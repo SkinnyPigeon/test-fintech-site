64e70299feb1b5d69a032026cc2fa7dc19d55e24
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-12-20 12:21:44 +0000

    getting the companies

--- client/src/models/MainView.js
+++ client/src/models/MainView.js
@@ -1,20 +1,32 @@
 var ElementMaker = require( './ElementMaker.js' );
 
 var MainView = function() {
+  this.companyUrl = "https://fintech-db-test.herokuapp.com/companys";
   this.companies = [];
 
   this.getCompanies();
-  this.show();
+  // this.show();
 }
 
 MainView.prototype = {
 
   getCompanies: function() {
-    
+    var request = new XMLHttpRequest();
+    request.open( 'GET', this.companyUrl );
+    request.setRequestHeader("Content-Type", "application/json")
+    request.withCredentials = true;
+    request.onload = () => {
+      if( request.status === 200 ) {
+        var companies = JSON.parse( request.responseText );
+        this.companies = companies;
+        this.show();
+      }
+    }
+    request.send( null );
   },
 
   show: function() {
-    console.log( "Hello" );
+    console.log( this.companies );
   }
 }
 
