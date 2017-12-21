65302868f6eac34b779ee9afa209b260e55dca73
Euan Blackledge <euanblackledge@Euans-MacBook-Pro.local>
2017-12-21 13:42:12 +0000

    detail view is coming along nicely

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -273,11 +273,24 @@
 	
 	DetailView.prototype = {
 	  show: function() {
+	    var basicMaker = new ElementMaker( 'detail-space', 'ul', 'companyBasicDetails' );
+	    basicMaker.makeList( this.company.name, 'companyBasicDetails' );
+	    basicMaker.makeList( this.company.phone, 'companyBasicDetails');
+	    basicMaker.makeList( this.company.email, 'companyBasicDetails' );
+	
+	    var addressMaker = new ElementMaker( 'detail-space', 'ul', 'companyAddress' );
+	    addressMaker.makeList( this.company.address_line_1, 'companyAddress' );
+	    addressMaker.makeList( this.company.address_line_2, 'companyAddress' );
+	    addressMaker.makeList( this.company.address_city, 'companyAddress' );
+	    addressMaker.makeList( this.company.address_region, 'companyAddress' );
+	    addressMaker.makeList( this.company.address_postcode, 'companyAddress' );
+	
 	    console.log( this.company );
 	  }
 	}
 	
 	module.exports = DetailView;
+
 
 /***/ },
 /* 6 */
